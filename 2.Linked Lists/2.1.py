d96baedb646bb72e6d4c7a315c43bd5eae51d5f3
shuoqi <qs0727@outlook.com>
2017-11-27 16:07:11 +0800

    2.1

---
+++ 2.Linked Lists/2.1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Question 2.1:
+    Remove Dups: Write code to remove duplicates from an unsorted linked list.
+                 FOLLOW UP
+                 How would you solve this problem if temporary buffer is not allowed.
+
+"""
