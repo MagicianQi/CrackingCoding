529f35f02058aef4eca89bef5f97d9a7eed73ce7
shuoqi <qs0727@outlook.com>
2017-12-01 12:14:28 +0800

    code

--- 2.Linked Lists/2.4.py
+++ 2.Linked Lists/2.4.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Question 2.3:
+Question 2.4:
     Partition: Write code to partition a linked list around a value x, such that all nodes less than x come
                before all nodes greater than or equal to x. if x is contained within the list, the values of
                x only need to be after the elements less than x(see below). The partition element x can appear
