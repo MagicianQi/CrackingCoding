63602541a3c402179e58c303271578b436d31d57
shuoqi <qs0727@outlook.com>
2018-01-08 11:31:04 +0800

    code

--- 4.Trees and Graphs/4.9.py
+++ 4.Trees and Graphs/4.9.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Question 4.8:
+Question 4.9:
     BST Sequences: A binary search tree was created by traversing through an array from left to right and inserting
                    each element. Given a binary search tree with distinct elements, print all possible arrays that
                    could have led to this tree.
